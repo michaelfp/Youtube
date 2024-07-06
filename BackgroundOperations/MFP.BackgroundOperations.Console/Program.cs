c75060edf35031d67cc1621e78e8e5f48d9eebd3
Michael Pereira <1903203+michaelfp@users.noreply.github.com>
2024-07-06 09:17:28 -0300

    Incluido projeto Background Operations

---
+++ BackgroundOperations/MFP.BackgroundOperations.Console/Program.cs
@@ -0,0 +1,219 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Services.Description;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace MFP.BackgroundOperations.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var crmclient = new CrmServiceClient(ConfigurationManager.ConnectionStrings["DataverseConnection"].ConnectionString);
+
+            Console.WriteLine("Pressione Qualquer tecla para iniciar:");
+            Console.ReadKey();
+            Console.WriteLine("Executando trabalho em segundo plano...");
+
+            var backgroundId = ExecutarTrabalhoSegundoPlano(crmclient, true);
+
+            var statusTrabalho = ConsultarStatusProcesso(crmclient, backgroundId);
+
+            while (statusTrabalho != BackgroundStatus.Succeeded && statusTrabalho != BackgroundStatus.Failed)
+            {
+                Thread.Sleep(5000);
+                statusTrabalho = ConsultarStatusProcesso(crmclient, backgroundId);
+            }
+
+            if (statusTrabalho == BackgroundStatus.Succeeded)
+            {
+                var respostaTrabalho = ObterRetornoProcesso(crmclient, backgroundId);
+
+                respostaTrabalho.ForEach(arg => Console.WriteLine($"Parâmetro: {arg.Key}: \r Valor: {arg.Value}"));
+            }
+
+            if (statusTrabalho == BackgroundStatus.Failed)
+            {
+                ConsultarErro(crmclient, backgroundId);
+            }
+            Console.ReadLine();
+        }
+
+        public static Guid ExecutarTrabalhoSegundoPlano(CrmServiceClient client, bool withCallback = false)
+        {
+
+            //Cria o request da action
+            var request = new OrganizationRequest("mfp_gerar_nota_fiscal")
+            {
+                Parameters = {
+                    {
+                        "Target", new EntityReference("mfp_pedidovenda", new Guid("b3b04ba8-3e3b-ef11-8409-000d3a1ccada"))
+
+
+                    }
+                    
+                }
+
+            };
+
+           
+
+            //Cria Request do trabalho em segundo plano
+            var requestBackground = new OrganizationRequest("ExecuteBackgroundOperation")
+            {
+                Parameters = {
+                    { "Request",request},
+                    {
+                        "CallbackUri", "https://webhook.site/0b414875-68ae-4958-bcc2-6dafaefcc812"
+                    }
+
+                }
+            };
+
+            //Executa o comando
+            var response = client.Execute(requestBackground);
+
+            var backgroundId = new Guid(response["BackgroundOperationId"].ToString());
+
+
+            Console.WriteLine($"BackgroundOperationId: {backgroundId}");
+
+            return backgroundId;
+        }
+
+        public static BackgroundStatus ConsultarStatusProcesso(CrmServiceClient client, Guid backgroundId)
+        {
+            var columnSet = new ColumnSet(
+                   "name",
+                   "backgroundoperationstatecode",
+                   "backgroundoperationstatuscode",
+                   "outputparameters",
+                   "errorcode",
+                   "errormessage");
+
+            try
+            {
+                // Get the entity with all the required columns
+                var backgroundOperation = client.Retrieve("backgroundoperation", backgroundId, columnSet);
+
+                Console.WriteLine($"Name: {backgroundOperation["name"]}");
+                Console.WriteLine($"State Code: {backgroundOperation.FormattedValues["backgroundoperationstatecode"]}");
+                Console.WriteLine($"Status Code: {backgroundOperation.FormattedValues["backgroundoperationstatuscode"]}");
+                Console.WriteLine(Environment.NewLine);
+
+                return (BackgroundStatus)backgroundOperation.GetAttributeValue<OptionSetValue>("backgroundoperationstatuscode").Value;
+
+            }
+            // Catch Dataverse errors
+            catch (FaultException<OrganizationServiceFault> ex)
+            {
+                Console.WriteLine($"ErrorCode:{ex.Detail.ErrorCode}");
+                Console.WriteLine($"Message:{ex.Detail.Message}");
+
+                return BackgroundStatus.Failed;
+            }
+            // Catch other errors
+            catch (Exception error)
+            {
+                Console.WriteLine($"Some other error occurred: '{error.Message}'");
+
+                return BackgroundStatus.Failed;
+            }
+        }
+
+
+        public static void ConsultarErro(CrmServiceClient client, Guid backgroundId)
+        {
+            var columnSet = new ColumnSet(
+                   "name",
+                   "errorcode",
+                   "errormessage");
+
+            try
+            {
+                // Get the entity with all the required columns
+                var backgroundOperation = client.Retrieve("backgroundoperation", backgroundId, columnSet);
+
+                Console.WriteLine($"Error Code: {backgroundOperation.GetAttributeValue<string>("errorcode")}");
+                Console.WriteLine($"Error Message: {backgroundOperation.GetAttributeValue<string>("errormessage")}");
+
+            }
+            // Catch Dataverse errors
+            catch (FaultException<OrganizationServiceFault> ex)
+            {
+                Console.WriteLine($"ErrorCode:{ex.Detail.ErrorCode}");
+                Console.WriteLine($"Message:{ex.Detail.Message}");
+
+            }
+            // Catch other errors
+            catch (Exception error)
+            {
+                Console.WriteLine($"Some other error occurred: '{error.Message}'");
+
+            }
+        }
+
+        public static List<KeyValuePair<string, string>> ObterRetornoProcesso(CrmServiceClient client, Guid backgroundId)
+        {
+            // List of columns that will help to get status, output and error details if any
+            var columnSet = new ColumnSet(
+                "name",
+                "outputparameters",
+                "errorcode"
+                );
+
+            try
+            {
+                // Get the entity with all the required columns
+                var backgroundOperation = client.Retrieve("backgroundoperation", backgroundId, columnSet);
+
+                Console.WriteLine($"Output Parameters:");
+
+                // Deserialize the Output Parameters into KeyValuePair<string, string>
+                List<KeyValuePair<string, string>> output =
+                    System.Text.Json.JsonSerializer
+                    .Deserialize<List<KeyValuePair<string, string>>>((string)backgroundOperation["outputparameters"]);
+
+                return output;
+            }
+            // Catch Dataverse errors
+            catch (FaultException<OrganizationServiceFault> ex)
+            {
+                Console.WriteLine($"ErrorCode:{ex.Detail.ErrorCode}");
+                Console.WriteLine($"Message:{ex.Detail.Message}");
+
+
+            }
+            // Catch other errors
+            catch (Exception error)
+            {
+                Console.WriteLine($"Some other error occurred: '{error.Message}'");
+            }
+
+            return null;
+        }
+    }
+
+    public enum BackgroundStatus
+    {
+        WaitingForResources = 0,
+        InProgress = 20,
+        Canceling = 22,
+        Succeeded = 30,
+        Failed = 31,
+        Canceled = 32
+
+    }
+
+
+}
---
+++ BackgroundOperations/MFP.BackgroundOperations/EnviarNotaFiscal.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MFP.BackgroundOperations
+{
+    public class EnviarNotaFiscal : IPlugin
+    {
+        IPluginExecutionContext context;
+        IOrganizationServiceFactory serviceFactory;
+        IOrganizationService orgService;
+        ITracingService tracingService;
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            orgService = serviceFactory.CreateOrganizationService(context.UserId);
+            tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+
+            if (context.MessageName.Equals("mfp_gerar_nota_fiscal"))
+            {
+                if (context.InputParameters.Contains("Target"))
+                {
+                    EntityReference orderParameter = null;
+
+                    context.InputParameters.TryGetValue<EntityReference>("Target", out orderParameter);
+
+                    if (orderParameter != null)
+                    {
+                        Entity order = orgService.Retrieve(orderParameter.LogicalName, orderParameter.Id, new Microsoft.Xrm.Sdk.Query.ColumnSet(true));
+
+                        if (order != null)
+                        {
+                            Thread.Sleep(14000);
+                            string xmlNota = ObterNota(order["mfp_numeropedido"].ToString());
+
+                            if (!string.IsNullOrEmpty(xmlNota))
+                            {
+                                order["mfp_nota_fiscal"] = xmlNota;
+
+                                orgService.Update(order);
+
+                                context.OutputParameters["resultado"] = "Nota fiscal gerada com sucesso!";
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+
+        public string ObterNota(string numeroPedido)
+        {
+            HttpClient client = new HttpClient();
+
+            HttpContent content = new StringContent($"{{\"numeroPedido\": \"{numeroPedido}\"}}", Encoding.UTF8, "application/json");
+
+            var responsePost = client.PostAsync($"https://webhook.site/f07a2a3d-6c0c-491b-aa30-850111c59459", content).Result;
+
+            if (responsePost.StatusCode != HttpStatusCode.OK)
+            {
+                throw new InvalidPluginExecutionException("Erro ao chamar serviço de geração de nota fiscal");
+            }
+
+            var jsonString = responsePost.Content.ReadAsStringAsync().Result;
+
+            tracingService.Trace("Response: " + jsonString);
+
+            ResponseNota nota = JsonSerializer.Deserialize<ResponseNota>(jsonString);
+
+            return nota.responseXML;
+
+
+        }
+    }
+
+    public class ResponseNota
+    {
+        public string responseXML { get; set; }
+    }
+}
